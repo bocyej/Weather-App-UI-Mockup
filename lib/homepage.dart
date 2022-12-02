18ed92a3b4b7b77978f34355d00b7c8029744f59
Jacob <jacobkristopherlaurel@gmail.com>
2022-12-02 23:55:28 +0800

    added list view

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
 
+import 'layouts/appbar.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: const Appbar(),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              scrollDirection: Axis.vertical,
+              children: [],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
