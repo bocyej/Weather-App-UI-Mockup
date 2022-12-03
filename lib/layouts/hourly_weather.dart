1b5f913825dd2e79d013e886f69564632fcbe333
Jacob <jacobkristopherlaurel@gmail.com>
2022-12-04 02:33:15 +0800

    created hourly weather

---
+++ lib/layouts/hourly_weather.dart
@@ -0,0 +1,296 @@
+import 'package:flutter/material.dart';
+
+class HourlyWeather extends StatelessWidget {
+  const HourlyWeather({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [
+          const Padding(padding: EdgeInsets.only(left: 10)),
+          InkWell(
+            onTap: () {
+              // Navigator.push(
+              //   context,
+              //   MaterialPageRoute(builder: (context) => const RewardsPage()),
+              // ),
+            },
+            child: Container(
+              height: 95,
+              child: Column(
+                children: [
+                  const Text(
+                    "2:00 PM",
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const Padding(padding: EdgeInsets.only(bottom: 10)),
+                  const Icon(
+                    Icons.thunderstorm,
+                    color: Colors.white,
+                    size: 35,
+                  ),
+                  const Padding(padding: EdgeInsets.only(bottom: 10)),
+                  InkWell(
+                    onTap: () {},
+                    child: Row(
+                      children: const [
+                        Text(
+                          "32°",
+                          style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const Padding(padding: EdgeInsets.only(right: 25)),
+          InkWell(
+            onTap: () {
+              // Navigator.push(
+              //   context,
+              //   MaterialPageRoute(builder: (context) => const RewardsPage()),
+              // ),
+            },
+            child: Container(
+              height: 95,
+              child: Column(
+                children: const [
+                  Text(
+                    "2:00 PM",
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Padding(padding: EdgeInsets.only(bottom: 10)),
+                  Icon(
+                    Icons.thunderstorm,
+                    color: Colors.white,
+                    size: 35,
+                  ),
+                  Padding(padding: EdgeInsets.only(bottom: 10)),
+                  Text(
+                    "32°",
+                    style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const Padding(padding: EdgeInsets.only(right: 25)),
+          InkWell(
+            onTap: () {
+              // Navigator.push(
+              //   context,
+              //   MaterialPageRoute(builder: (context) => const RewardsPage()),
+              // ),
+            },
+            child: Container(
+              height: 95,
+              child: Column(
+                children: const [
+                  Text(
+                    "2:00 PM",
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Padding(padding: EdgeInsets.only(bottom: 10)),
+                  Icon(
+                    Icons.thunderstorm,
+                    color: Colors.white,
+                    size: 35,
+                  ),
+                  Padding(padding: EdgeInsets.only(bottom: 10)),
+                  Text(
+                    "32°",
+                    style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const Padding(padding: EdgeInsets.only(right: 25)),
+          InkWell(
+            onTap: () {
+              // Navigator.push(
+              //   context,
+              //   MaterialPageRoute(builder: (context) => const RewardsPage()),
+              // ),
+            },
+            child: Container(
+              height: 95,
+              child: Column(
+                children: const [
+                  Text(
+                    "2:00 PM",
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Padding(padding: EdgeInsets.only(bottom: 10)),
+                  Icon(
+                    Icons.thunderstorm,
+                    color: Colors.white,
+                    size: 35,
+                  ),
+                  Padding(padding: EdgeInsets.only(bottom: 10)),
+                  Text(
+                    "32°",
+                    style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const Padding(padding: EdgeInsets.only(right: 25)),
+          InkWell(
+            onTap: () {
+              // Navigator.push(
+              //   context,
+              //   MaterialPageRoute(builder: (context) => const RewardsPage()),
+              // ),
+            },
+            child: Container(
+              height: 95,
+              child: Column(
+                children: const [
+                  Text(
+                    "2:00 PM",
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Padding(padding: EdgeInsets.only(bottom: 10)),
+                  Icon(
+                    Icons.thunderstorm,
+                    color: Colors.white,
+                    size: 35,
+                  ),
+                  Padding(padding: EdgeInsets.only(bottom: 10)),
+                  Text(
+                    "32°",
+                    style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const Padding(padding: EdgeInsets.only(right: 25)),
+          InkWell(
+            onTap: () {
+              // Navigator.push(
+              //   context,
+              //   MaterialPageRoute(builder: (context) => const RewardsPage()),
+              // ),
+            },
+            child: Container(
+              height: 95,
+              child: Column(
+                children: const [
+                  Text(
+                    "2:00 PM",
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Padding(padding: EdgeInsets.only(bottom: 10)),
+                  Icon(
+                    Icons.thunderstorm,
+                    color: Colors.white,
+                    size: 35,
+                  ),
+                  Padding(padding: EdgeInsets.only(bottom: 10)),
+                  Text(
+                    "32°",
+                    style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const Padding(padding: EdgeInsets.only(right: 25)),
+          InkWell(
+            onTap: () {
+              // Navigator.push(
+              //   context,
+              //   MaterialPageRoute(builder: (context) => const RewardsPage()),
+              // ),
+            },
+            child: Container(
+              height: 95,
+              child: Column(
+                children: const [
+                  Text(
+                    "2:00 PM",
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Padding(padding: EdgeInsets.only(bottom: 10)),
+                  Icon(
+                    Icons.thunderstorm,
+                    color: Colors.white,
+                    size: 35,
+                  ),
+                  Padding(padding: EdgeInsets.only(bottom: 10)),
+                  Text(
+                    "32°",
+                    style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const Padding(padding: EdgeInsets.only(right: 10)),
+        ],
+      ),
+    );
+  }
+}
