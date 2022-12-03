2721e45d46269f66148f2c70fcfd4bd386359af6
Jacob <jacobkristopherlaurel@gmail.com>
2022-12-04 02:33:37 +0800

    created current weather

---
+++ lib/layouts/current_weather.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class CurrentWeather extends StatelessWidget {
+  const CurrentWeather({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        // Navigator.push(
+        //   context,
+        //   MaterialPageRoute(builder: (context) => const RewardsPage()),
+        // ),
+      },
+      child: Container(
+        height: 150,
+        child: Center(
+          child: Column(
+            children: const [
+              Text(
+                "27°",
+                style: TextStyle(
+                  fontSize: 80,
+                  fontWeight: FontWeight.w200,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                "Mostly Cloudy",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white,
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 10)),
+              Text(
+                "32° /  25°",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
