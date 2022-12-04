4f39cebe1616b88a8ea57ea7b357fe64ffa3ae64
Jacob <jacobkristopherlaurel@gmail.com>
2022-12-05 00:35:39 +0800

    removed container and inkwell on temperature

--- lib/layouts/hourly_weather.dart
+++ lib/layouts/hourly_weather.dart
@@ -5,58 +5,53 @@ class HourlyWeather extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        children: [
-          const Padding(padding: EdgeInsets.only(left: 10)),
-          InkWell(
-            onTap: () {
-              // Navigator.push(
-              //   context,
-              //   MaterialPageRoute(builder: (context) => const RewardsPage()),
-              // ),
-            },
-            child: Container(
-              height: 95,
-              child: Column(
-                children: [
-                  const Text(
-                    "2:00 PM",
-                    style: TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.w400,
-                      color: Colors.white,
-                    ),
-                  ),
-                  const Padding(padding: EdgeInsets.only(bottom: 10)),
-                  const Icon(
-                    Icons.thunderstorm,
+    return Row(
+      children: [
+        const Padding(padding: EdgeInsets.only(left: 10)),
+        InkWell(
+          onTap: () {
+            // Navigator.push(
+            //   context,
+            //   MaterialPageRoute(builder: (context) => const RewardsPage()),
+            // ),
+          },
+          child: Container(
+            height: 95,
+            child: Column(
+              children: [
+                const Text(
+                  "2:00 PM",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400,
                     color: Colors.white,
-                    size: 35,
                   ),
-                  const Padding(padding: EdgeInsets.only(bottom: 10)),
-                  InkWell(
-                    onTap: () {},
-                    child: Row(
-                      children: const [
-                        Text(
-                          "32°",
-                          style: TextStyle(
-                            fontSize: 17,
-                            fontWeight: FontWeight.w400,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
+                ),
+                const Padding(padding: EdgeInsets.only(bottom: 10)),
+                const Icon(
+                  Icons.thunderstorm,
+                  color: Colors.white,
+                  size: 35,
+                ),
+                const Padding(padding: EdgeInsets.only(bottom: 10)),
+                Row(
+                  children: const [
+                    Text(
+                      "32°",
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.white,
+                      ),
                     ),
-                  ),
-                ],
-              ),
+                  ],
+                ),
+              ],
             ),
           ),
-          const Padding(padding: EdgeInsets.only(right: 10)),
-        ],
-      ),
+        ),
+        const Padding(padding: EdgeInsets.only(right: 10)),
+      ],
     );
   }
 }
