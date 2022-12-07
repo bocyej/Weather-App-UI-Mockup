1af8ab63ecfae46b04690968d9ba9a647b28894b
Jacob <jacobkristopherlaurel@gmail.com>
2022-12-07 15:30:38 +0800

    added comments about the date increments

--- lib/layouts/date_time.dart
+++ lib/layouts/date_time.dart
@@ -3,14 +3,28 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class DateAndTime extends StatelessWidget {
-  const DateAndTime({super.key});
+  DateAndTime({super.key});
+
+  final today = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
+    final todayadd = today.add(Duration(days: 1));
+
     return Container(
-      child: Text(
-        DateFormat('EEE, MMMM d').format(DateTime.now()),
+      child: Column(
+        children: [
+          Text(
+            DateFormat('EEE, MMMM d').format(today),
+          ),
+          Text(
+            DateFormat('EEE, MMMM d').format(todayadd),
+          ),
+        ],
       ),
     );
   }
 }
+
+// Not in use in the Weather App UI
+// Use for reference when incrementing specific dates
