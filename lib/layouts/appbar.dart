826e3c72ced5e072a4b819dd4d8485731621af85
Jacob <jacobkristopherlaurel@gmail.com>
2022-12-07 16:27:04 +0800

    changed title font size

--- lib/layouts/appbar.dart
+++ lib/layouts/appbar.dart
@@ -25,7 +25,7 @@ class Appbar extends StatelessWidget with PreferredSizeWidget {
           child: Text(
             "Manila",
             style: TextStyle(
-              fontSize: 20,
+              fontSize: 23,
               fontWeight: FontWeight.w400,
               color: Colors.white,
             ),
