48d73f89629d6166087025816d778e754684ea31
Jacob <jacobkristopherlaurel@gmail.com>
2022-12-07 15:11:46 +0800

    removed theme data in main dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Weather',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       home: HomePage(),
     );
   }
