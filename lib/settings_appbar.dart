4a833acba536b87e0f5fefdd57c4617df15f2c15
Jacob <jacobkristopherlaurel@gmail.com>
2022-12-08 21:26:45 +0800

    created settings appbar

---
+++ lib/settings_appbar.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/pages/homepage.dart';
+
+class Appbar extends StatelessWidget with PreferredSizeWidget {
+  const Appbar({super.key});
+
+  Size get preferredSize => const Size.fromHeight(50);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      elevation: 0,
+      flexibleSpace: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage('assets/appbar_night_sky.jpg'),
+          fit: BoxFit.fill,
+        )),
+      ),
+      leading: IconButton(
+        icon: const Icon(Icons.arrow_back_ios),
+        tooltip: 'Back',
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const HomePage()),
+          );
+        },
+      ),
+      title: const Text(
+        "Settings",
+        style: TextStyle(
+          fontSize: 23,
+          fontWeight: FontWeight.w400,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
