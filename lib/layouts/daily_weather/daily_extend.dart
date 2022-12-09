4ec85ec7c458cc3fb59dedaf451863a915e0a8f4
Jacob <jacobkristopherlaurel@gmail.com>
2022-12-10 00:58:58 +0800

    moved sizedbox to column, added divider

--- lib/layouts/daily_weather/daily_extend.dart
+++ lib/layouts/daily_weather/daily_extend.dart
@@ -5,21 +5,31 @@ class DailyExtend extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 45,
-      child: InkWell(
-        onTap: () {},
-        child: const Center(
-          child: Text(
-            "Extended Forecast",
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.w400,
-              color: Colors.white54,
+    return Column(
+      children: [
+        const Divider(
+          color: Colors.white38,
+          thickness: 1,
+          indent: 20,
+          endIndent: 20,
+        ),
+        SizedBox(
+          height: 45,
+          child: InkWell(
+            onTap: () {},
+            child: const Center(
+              child: Text(
+                "Extended Forecast",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white54,
+                ),
+              ),
             ),
           ),
         ),
-      ),
+      ],
     );
   }
 }
