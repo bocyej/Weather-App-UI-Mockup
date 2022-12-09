987e55b2e98a405276f1288e16fee9347e205a58
Jacob <jacobkristopherlaurel@gmail.com>
2022-12-10 00:54:59 +0800

    renamed to dw 1, icon to image, divider, padding

--- lib/layouts/daily_weather/daily_weather1.dart
+++ lib/layouts/daily_weather/daily_weather1.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 
-class DailyWeather extends StatelessWidget {
-  const DailyWeather({super.key});
+class DailyWeatherOne extends StatelessWidget {
+  const DailyWeatherOne({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
         children: [
+          const Divider(
+            color: Colors.white38,
+            thickness: 1,
+            indent: 20,
+            endIndent: 20,
+          ),
+          const Padding(padding: EdgeInsets.only(bottom: 10)),
           InkWell(
             onTap: () {},
-            child: Container(
+            child: SizedBox(
               height: 35,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -24,10 +31,9 @@ class DailyWeather extends StatelessWidget {
                     ),
                   ),
                   const Padding(padding: EdgeInsets.only(right: 10)),
-                  const Icon(
-                    Icons.thunderstorm,
-                    color: Colors.white,
-                    size: 35,
+                  const Image(
+                    image: AssetImage("assets/cloud_thunder.png"),
+                    height: 35,
                   ),
                   const Padding(padding: EdgeInsets.only(right: 10)),
                   Row(
@@ -54,6 +60,7 @@ class DailyWeather extends StatelessWidget {
               ),
             ),
           ),
+          const Padding(padding: EdgeInsets.only(bottom: 10)),
         ],
       ),
     );
