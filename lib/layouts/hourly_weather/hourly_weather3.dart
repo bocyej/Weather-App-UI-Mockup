f280b3dd9fe3ca437bd96b218ffefaa98baf1c9d
Jacob <jacobkristopherlaurel@gmail.com>
2022-12-09 17:40:51 +0800

    created hourly weather 3

---
+++ lib/layouts/hourly_weather/hourly_weather3.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class HourlyWeatherThree extends StatelessWidget {
+  const HourlyWeatherThree({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const Padding(padding: EdgeInsets.only(left: 10)),
+        InkWell(
+          onTap: () {},
+          child: Container(
+            height: 95,
+            child: Column(
+              children: [
+                const Text(
+                  "Now",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.white,
+                  ),
+                ),
+                const Padding(padding: EdgeInsets.only(bottom: 10)),
+                const Image(
+                  image: AssetImage("assets/cloud_moon.png"),
+                  height: 35,
+                ),
+                const Padding(padding: EdgeInsets.only(bottom: 10)),
+                Row(
+                  children: const [
+                    Text(
+                      "26°",
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+        const Padding(padding: EdgeInsets.only(right: 10)),
+      ],
+    );
+  }
+}
