755a82a2fdc0f0b19772f2625e15469cfb483b7c
Jacob <jacobkristopherlaurel@gmail.com>
2022-12-10 00:55:30 +0800

    created daily weather 4

---
+++ lib/layouts/daily_weather/daily_weather4.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class DailyWeatherFour extends StatelessWidget {
+  const DailyWeatherFour({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          const Divider(
+            color: Colors.white38,
+            thickness: 1,
+            indent: 20,
+            endIndent: 20,
+          ),
+          const Padding(padding: EdgeInsets.only(bottom: 10)),
+          InkWell(
+            onTap: () {},
+            child: SizedBox(
+              height: 35,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  const Text(
+                    "Tue, Dec 6",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const Padding(padding: EdgeInsets.only(right: 10)),
+                  const Image(
+                    image: AssetImage("assets/cloud_sun.png"),
+                    height: 35,
+                  ),
+                  const Padding(padding: EdgeInsets.only(right: 10)),
+                  Row(
+                    children: const [
+                      Text(
+                        "32° ",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        "/ 25°",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.white54,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const Padding(padding: EdgeInsets.only(bottom: 10)),
+        ],
+      ),
+    );
+  }
+}
