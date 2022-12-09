dbedd18fe1f032dd95781cf6e3c36f540338e468
Jacob <jacobkristopherlaurel@gmail.com>
2022-12-10 00:55:38 +0800

    created daily weather 5

---
+++ lib/layouts/daily_weather/daily_weather5.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class DailyWeatherFive extends StatelessWidget {
+  const DailyWeatherFive({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          const Divider(
+            color: Colors.white38,
+            thickness: 1,
+            indent: 20,
+            endIndent: 20,
+          ),
+          const Padding(padding: EdgeInsets.only(bottom: 10)),
+          InkWell(
+            onTap: () {},
+            child: SizedBox(
+              height: 35,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  const Text(
+                    "Wed, Dec 7",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const Padding(padding: EdgeInsets.only(right: 10)),
+                  const Icon(
+                    Icons.thunderstorm,
+                    color: Colors.white,
+                    size: 35,
+                  ),
+                  const Padding(padding: EdgeInsets.only(right: 10)),
+                  Row(
+                    children: const [
+                      Text(
+                        "32° ",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        "/ 25°",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.white54,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const Padding(padding: EdgeInsets.only(bottom: 10)),
+        ],
+      ),
+    );
+  }
+}
