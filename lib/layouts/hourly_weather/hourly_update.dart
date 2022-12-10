617de03815372061c8b12d9edfa708a3d6ec614b
Jacob <jacobkristopherlaurel@gmail.com>
2022-12-11 00:54:42 +0800

    changed container to column, added divider

--- lib/layouts/hourly_weather/hourly_update.dart
+++ lib/layouts/hourly_weather/hourly_update.dart
@@ -5,44 +5,52 @@ class HourlyUpdate extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          const Padding(padding: EdgeInsets.only(left: 20)),
-          const Text(
-            "Updated 10:00 PM",
-            style: TextStyle(
-              fontSize: 15,
-              fontWeight: FontWeight.w400,
-              color: Colors.white38,
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const Padding(padding: EdgeInsets.only(left: 20)),
+            const Text(
+              "Updated 10:00 PM",
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.w400,
+                color: Colors.white38,
+              ),
             ),
-          ),
-          const Padding(padding: EdgeInsets.only(left: 135)),
-          InkWell(
-            onTap: () {},
-            child: Row(
-              children: const [
-                Icon(
-                  Icons.wb_sunny,
-                  size: 15,
-                  color: Colors.white38,
-                ),
-                Padding(padding: EdgeInsets.only(right: 5)),
-                Text(
-                  "AccuWeather",
-                  style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.w400,
+            const Padding(padding: EdgeInsets.only(left: 135)),
+            InkWell(
+              onTap: () {},
+              child: Row(
+                children: const [
+                  Icon(
+                    Icons.wb_sunny,
+                    size: 15,
                     color: Colors.white38,
                   ),
-                ),
-              ],
+                  Padding(padding: EdgeInsets.only(right: 5)),
+                  Text(
+                    "AccuWeather",
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white38,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          const Padding(padding: EdgeInsets.only(right: 20)),
-        ],
-      ),
+            const Padding(padding: EdgeInsets.only(right: 20)),
+          ],
+        ),
+        const Divider(
+          color: Colors.white38,
+          thickness: 1,
+          indent: 20,
+          endIndent: 20,
+        ),
+      ],
     );
   }
 }
