8a50980e832540e03019481b55eabb5ec99b3907
Jacob <jacobkristopherlaurel@gmail.com>
2022-12-12 17:22:13 +0800

    moved and imported new layouts in settings

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
-import 'package:weather_app/layouts/settings_appbar.dart';
-import 'package:weather_app/layouts/toggle_switch.dart';
+import 'package:weather_app/layouts/settings/settings_appbar.dart';
+import 'package:weather_app/layouts/settings/settings_sounds.dart';
+import 'package:weather_app/layouts/settings/settings_autoupdate.dart';
+import 'package:weather_app/layouts/settings/settings_temperature.dart';
+import 'package:weather_app/layouts/settings/settings_updateinterval.dart';
+import 'package:weather_app/layouts/settings/settings_about.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
@@ -18,175 +22,12 @@ class SettingsPage extends StatelessWidget {
           ),
         ),
         child: Column(
-          children: [
-            const Divider(
-              color: Colors.white38,
-              thickness: 1,
-              indent: 20,
-              endIndent: 20,
-            ),
-            const Padding(padding: EdgeInsets.only(bottom: 10)),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  "Temperature Unit",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.white,
-                  ),
-                ),
-                const Padding(padding: EdgeInsets.only(right: 110)),
-                InkWell(
-                  onTap: () {},
-                  child: Row(
-                    children: const [
-                      Text(
-                        "Auto (°C)",
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.white,
-                        ),
-                      ),
-                      Padding(padding: EdgeInsets.only(right: 5)),
-                      Icon(
-                        Icons.arrow_forward_ios,
-                        color: Colors.white38,
-                        size: 20,
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            const Padding(padding: EdgeInsets.only(bottom: 10)),
-            const Divider(
-              color: Colors.white38,
-              thickness: 1,
-              indent: 20,
-              endIndent: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Text(
-                  "Weather Sounds",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.white,
-                  ),
-                ),
-                Padding(padding: EdgeInsets.only(right: 165)),
-                ToggleSwitch(),
-              ],
-            ),
-            const Divider(
-              color: Colors.white38,
-              thickness: 1,
-              indent: 20,
-              endIndent: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Text(
-                  "Auto Update",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.white,
-                  ),
-                ),
-                Padding(padding: EdgeInsets.only(right: 200)),
-                ToggleSwitch(),
-              ],
-            ),
-            const Divider(
-              color: Colors.white38,
-              thickness: 1,
-              indent: 20,
-              endIndent: 20,
-            ),
-            const Padding(padding: EdgeInsets.only(bottom: 10)),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  "Update Interval",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.white,
-                  ),
-                ),
-                const Padding(padding: EdgeInsets.only(right: 90)),
-                InkWell(
-                  onTap: () {},
-                  child: Row(
-                    children: const [
-                      Text(
-                        "Every 6 hours",
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.white,
-                        ),
-                      ),
-                      Padding(padding: EdgeInsets.only(right: 5)),
-                      Icon(
-                        Icons.arrow_forward_ios,
-                        color: Colors.white38,
-                        size: 20,
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            const Padding(padding: EdgeInsets.only(bottom: 10)),
-            const Divider(
-              color: Colors.white38,
-              thickness: 1,
-              indent: 20,
-              endIndent: 20,
-            ),
-            const Padding(padding: EdgeInsets.only(bottom: 10)),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  "About",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.white,
-                  ),
-                ),
-                const Padding(padding: EdgeInsets.only(right: 295)),
-                InkWell(
-                  onTap: () {},
-                  child: Row(
-                    children: const [
-                      Icon(
-                        Icons.arrow_forward_ios,
-                        color: Colors.white38,
-                        size: 20,
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            const Padding(padding: EdgeInsets.only(bottom: 10)),
-            const Divider(
-              color: Colors.white38,
-              thickness: 1,
-              indent: 20,
-              endIndent: 20,
-            ),
+          children: const [
+            TemperatureUnit(),
+            WeatherSounds(),
+            AutoUpdate(),
+            UpdateInterval(),
+            About(),
           ],
         ),
       ),
